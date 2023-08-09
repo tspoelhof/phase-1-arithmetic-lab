ecf63ade7cc70bf79265bf63b22d9227b6a99541
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-08 23:18:51 -0400

    a

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+//function multiply(num1 = 31, num2 = 2) {
+    //return num1 * num2;
+//}
+/*const num1 = 31;
+const num2 = 2;
+
+const result = multiply(num1, num2);
+console.log(result);
+*/
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() + 1);
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const numberList = [2, 10, 12, 20];
+const max = Math.max(...numberList);
